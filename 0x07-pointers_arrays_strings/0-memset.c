8be498ff137619d6932af9ad2beb815065bb23da
Hamdy Farag <fargh1597@gmail.com>
2023-07-03 14:02:02 -0700

    first one

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _memset - memestsdg
+ * @s: sdgsdg
+ * @b: dsagdsag
+ * @n: adgsdg
+ *
+ *
+ *
+ * Return: iiiii iii
+*/
+
+
+
+
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i, j;
+
+	j = n;
+
+	for (i = 0; i < j; i++)
+	{
+		*s = b;
+		s++;
+	}
+	return (s);
+}
