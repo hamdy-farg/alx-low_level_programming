93e52b74d4b332e2a63be2c9f7e350769cd4164f
Hamdy Farag <fargh1597@gmail.com>
2023-07-03 18:06:04 -0700

    dd

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - dsfas
+ * @s: dfsadfadsf
+ * @accept: dagdsaf
+ *
+ * Return: asgfdafdasfsdf
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				p = &s[i];
+				return (p);
+			}
+		}
+	}
+	return (0);
+}
