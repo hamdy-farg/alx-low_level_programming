af0399d22a26193ddf7b100980f40d15674abf08
Hamdy Farag <fargh1597@gmail.com>
2023-07-07 18:19:37 -0700

    dos

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - mian
+ * @argc: adsgdsg
+ * @argv: list
+ * Return: 0
+ *
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%S\n", argv[0]);
+	return (0);
+}
