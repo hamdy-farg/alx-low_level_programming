d1d2cfa04256ac941a5ac6e818c64dcf04258083
Hamdy Farag <fargh1597@gmail.com>
2023-07-11 15:22:41 -0700

    dasg

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - sdagdsg
+ * @width: sdgdsg
+ * @height: sdgsdg
+ *
+ *
+ *
+ * Return: dsagdsg
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **t, i, j;
+
+	t = malloc(sizeof(int) * height);
+
+	if (width > 0 || height > 0 || t != 0)
+	{
+		for (i = 0; height > i; i++)
+		{
+			t[i] = malloc(sizeof(**t) * width);
+			if (t[i] == 0)
+			{
+				while (i--)
+				{
+					free(t[i]);
+				}
+				free(t);
+				return (NULL);
+			}
+			for (j = 0; width > j; j++)
+			{
+				t[i][j] = 0;
+			}
+		}
+
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (t);
+
+}
+
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - frees the grid
+ *
+ * @grid: edasgg
+ * @height: sadgdsahg
+ *
+ *
+ *
+ *
+ * Return: asdgdasgads
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for(i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
+
