f913ad9ea2991672ed9068ad5a146de550acf666
Hamdy Farag <fargh1597@gmail.com>
2023-07-11 14:35:31 -0700

    this is my commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,55 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * str_concat - concatonate to function
- * @s1: si si
- * @s2: s2 s2
- *
- *
- *
- * Return: asdfasddasg
-*/
-
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
+ */
 char *str_concat(char *s1, char *s2)
 {
-	char  *s3;
-	
-	int t, i, j, sum;
+	char *conct;
+	int i, ci;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
-	
-	for (i = 0; s1[i] != '\0'; i++)
-	;
+		s2 = "";
 
-	for (j = 0; s2[j] != '\0'; j++)
-	;
-	
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-	sum = i + j  ;
-	
-	s3 = malloc(1 + sizeof(char) * sum);
-	
-	if (s3 == 0)
+	if (conct == NULL)
+		return (NULL);
+	i = ci = 0;
+	while (s1[i] != '\0')
 	{
-		return (0);
+		conct[i] = s1[i];
+		i++;
 	}
 
-	for (t = 0; t <= sum ; t++)
+	while (s2[ci] != '\0')
 	{
-		if (t < i)
-		{
-			s3[t] = s1[t];
-		}
-		else
-		{
-			s3[t] = s2[t - i];
-		}
+		conct[i] = s2[ci];
+		i++, ci++;
 	}
-	s3[t] = '\0';
-	return (s3);
-
+	conct[i] = '\0';
+	return (conct);
 }
