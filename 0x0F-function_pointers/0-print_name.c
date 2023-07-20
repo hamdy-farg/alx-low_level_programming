ed0ef3038701be0f38065f03a1ac01f092ae6c08
Hamdy Farag <fargh1597@gmail.com>
2023-07-19 18:32:26 -0700

    sexom

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,6 +5,7 @@
 * print_name - print a name
 * @name: the string name
 * @f: the printing function pointer
+*
 * Return: void
 */
 void print_name(char *name, void (*f)(char *))
@@ -12,3 +13,4 @@ void print_name(char *name, void (*f)(char *))
 	if (name && f)
 		f(name);
 }
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "calc.h"
+
+/**
+* op_add - sfdhgds
+* @a: adsg
+* @b: dsag
+* Return: sdg
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - sfdhdgds
+* @a: adfsg
+* @b: dsagg
+* Return: sgdg
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - sfadhgds
+* @a: adsgg
+* @b: dsahg
+* Return: sadg
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - sefdhgds
+* @a: adsgt
+* @b: dsage
+* Return: stdg
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - sfgdhgds
+* @a: adsge
+* @b: dsaga
+* Return: sddg
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
