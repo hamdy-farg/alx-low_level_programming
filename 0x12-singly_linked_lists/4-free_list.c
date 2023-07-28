1ef1ec00970e192b2633da99891ae013f3fa82a2
Hamdy <fargh1597@gmail.com>
2023-07-27 18:21:56 -0700

    second

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists"
+
+/**
+* free_list - frees a linked list
+* @head: dsg
+*
+*
+*/
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
