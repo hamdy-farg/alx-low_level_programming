657ad003ab822011cb3ff6eb3a3484364e21c1a7
Hamdy <fargh1597@gmail.com>
2023-08-07 18:27:24 -0700

    df

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* ssize_t read_textfil - ffff
+* @filename: dsag
+* @letters: gggggg
+* Return: fffff
+*
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	close(fd);
+	return (bytes);
+}
